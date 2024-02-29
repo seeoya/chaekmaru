32b4d9b9b0d9987531283a052c541efb60c9b08d
post50 <ppm4ppm4@gmail.com>
2024-02-29 18:22:49 +0900

    config 상수 추가, admin member java, html, js 작성

--- src/main/java/com/maru/chaekmaru/admin/member/AdminMemberService.java
+++ src/main/java/com/maru/chaekmaru/admin/member/AdminMemberService.java
@@ -1,5 +1,79 @@
 package com.maru.chaekmaru.admin.member;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maru.chaekmaru.config.Config;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Service
 public class AdminMemberService {
+
+	@Autowired
+	IAdminMemberDaoForMybatis adminMemberDao;
+	
+	public int createAccountConfirm(AdminMemberDto adminMemberDto) {
+		log.info("createAccountConfirm()");
+		
+		boolean isAdminMember = adminMemberDao.isAdminMember(adminMemberDto.getA_id());
+		
+		if(!isAdminMember) {
+			int result = adminMemberDao.insertAdminAccount(adminMemberDto);
+			
+			if(result > 0)
+				return Config.INSERT_SUCCESS_AT_DATABASE;
+			else 
+				return Config.INSERT_FAIL_AT_DATABASE;
+			
+		} else {
+			return Config.ID_ALREADY_EXIST;
+		}
+		
+	}
+
+	public AdminMemberDto loginConfirm(AdminMemberDto adminMemberDto) {
+		log.info("loginConfirm()");
+		
+		return adminMemberDao.selectAdmin(adminMemberDto);
+	}
+
+	public int modifyAccountConfirm(AdminMemberDto adminMemberDto) {
+		log.info("modifyAccountConfirm");
+		
+		return adminMemberDao.updateAdminForModify(adminMemberDto);
+	}
+
+	public int deleteAccountConfirm(int a_no) {
+		log.info("deleteAccountConfirm");
+		
+		int result = adminMemberDao.deleteAdminAccount(a_no);
+		
+		if(result > 0)
+			return Config.DELETE_SUCCESS_AT_DATABASE;
+		else 
+			return Config.DELETE_FAIL_AT_DATABASE;		
+		
+	}
+
+	public List<AdminMemberDto> adminListForm() {
+		log.info("adminListForm()");
+		
+		return adminMemberDao.selectAllAdmins();
+	}
+
+	public int setAdminApproval(int a_no) {
+		log.info("setAdminApproval");
+		
+		int result = adminMemberDao.updateAdminApproval(a_no);
+		
+		if(result > 0)
+			return Config.SET_ADMIN_APPROVAL_SUCCESS_AT_DATABASE;
+		else 
+			return Config.SET_ADMIN_APPROVAL_FAIL_AT_DATABASE;		
+	}
 
 }
