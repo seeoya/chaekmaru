ffa19e832cf51defd369ffaf94c06e4c32a153f4
post50 <ppm4ppm4@gmail.com>
2024-02-29 15:17:20 +0900

    admin_dao_mapper, 인터페이스 작성, interceptor werbMvcConfig작성

--- src/main/java/com/maru/chaekmaru/admin/AdminMemberDto.java
+++ src/main/java/com/maru/chaekmaru/admin/AdminMemberDto.java
@@ -3,14 +3,14 @@ package com.maru.chaekmaru.admin;
 import lombok.Data;
 
 @Data
-public class AdminDto {
+public class AdminMemberDto {
 	
 	private int a_no;
 	private String a_id;
+	private String a_pw;	
 	private int a_grade;
 	private String a_name;
-	private String a_pw;
-	private String a_log_date;
+	private String a_phone;	
 	private String a_reg_date;
 	private String a_mod_date;
 	
--- src/main/java/com/maru/chaekmaru/admin/member/AdminMemberDto.java
+++ src/main/java/com/maru/chaekmaru/admin/member/AdminMemberDto.java
@@ -1,5 +1,18 @@
 package com.maru.chaekmaru.admin.member;
 
+import lombok.Data;
+
+@Data
 public class AdminMemberDto {
+	
+	private int a_no;
+	private String a_id;
+	private int a_grade;
+	private String a_name;
+	private String a_pw;
+	private String a_log_date;
+	private String a_reg_date;
+	private String a_mod_date;
+	
 
 }
