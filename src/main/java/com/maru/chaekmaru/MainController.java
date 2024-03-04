adda96d43b8f2ff422a61296b3642d1bb6feea6f
seeoya <seeoya@naver.com>
2024-03-04 13:01:26 +0900

    메인 컨트롤러 미사용 import 제거

--- src/main/java/com/maru/chaekmaru/MainController.java
+++ src/main/java/com/maru/chaekmaru/MainController.java
@@ -1,23 +1,11 @@
 package com.maru.chaekmaru;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.maru.chaekmaru.book.BookDto;
-import com.maru.chaekmaru.member.MemberDto;
 
 import lombok.extern.log4j.Log4j2;
 
