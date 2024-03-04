2eba467c539ef8ac11d6568541fab52f1a8ac861
seeoya <seeoya@naver.com>
2024-03-04 18:01:13 +0900

    메인 작업 1차 완료

--- src/main/java/com/maru/chaekmaru/MainController.java
+++ src/main/java/com/maru/chaekmaru/MainController.java
@@ -1,11 +1,16 @@
 package com.maru.chaekmaru;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.maru.chaekmaru.book.BookDto;
+import com.maru.chaekmaru.book.BookService;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -17,11 +22,23 @@ public class MainController {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 
+	@Autowired
+	BookService bookService;
+
 	String nextPage = "";
 
 	@GetMapping({ "/", "" })
 	public String index(Model model) {
 		nextPage = "index";
+
+		ArrayList<BookDto> recommendBookDtos  = bookService.recommendItem();
+		model.addAttribute("recommend", recommendBookDtos);
+
+		ArrayList<BookDto> bestBookDtos = bookService.bestItem(5);
+		model.addAttribute("best", bestBookDtos);
+
+		ArrayList<BookDto> newBookDtos = bookService.newItem(5);
+		model.addAttribute("news", newBookDtos);
 
 		return nextPage;
 	}
