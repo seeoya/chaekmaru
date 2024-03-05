16fdde8c69e7a55a16cbddee8fb880afbb2564aa
Choi <chm9656@gmail.com>
2024-03-05 12:50:11 +0900

    TBL_SALED_BOOK에 SB_RETURN_COUNT 추가, SB_STATE NOT NULL 추가  / dto 오타 수정(thumnail -> thumbnail , intorduce -> introduce) 및 sb_return_count 컬럼 추가 / MyPointListDto에 TBL_SALED_BOOK, TBL_BOOK 컬럼 추가

--- src/main/java/com/maru/chaekmaru/review/ReviewDto.java
+++ src/main/java/com/maru/chaekmaru/review/ReviewDto.java
@@ -26,10 +26,10 @@ public class ReviewDto {
 	private String m_mod_date;
 	
 	private int b_no;
-	private String b_thumnail;
+	private String b_thumbnail;
 	private String b_name;
 	private String b_author;
-	private String b_intorduce;
+	private String b_introduce;
 	private String b_publisher;
 	private String b_publish_date;
 	private String b_kdc;
