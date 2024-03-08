a401b8ba21aea9e6a6527e99aefa16ea369721f8
Choi <chm9656@gmail.com>
2024-03-08 18:45:12 +0900

    TBL_MEMBER M_ADDR_CODE, M_ADDR NOT NULL 추가  / TBL_SALED_BOOK SB_COUPON CHAR(1) -> SB_SALED_PRICE  NUMBER(7) 변경 / TBL_ATTEND_CHECK 추가 및 MemberAttendanceDto 추가

---
+++ src/main/java/com/maru/chaekmaru/member/MemberAttendanceDto.java
@@ -0,0 +1,24 @@
+package com.maru.chaekmaru.member;
+
+import lombok.Data;
+
+@Data
+public class MemberAttendanceDto {
+	
+	private int ac_attend_date;
+	private String ac_reg_date;
+	
+	private String m_id;
+	private String m_pw;
+	private String m_name;
+	private String m_mail;
+	private String m_phone;
+	private String m_addr_code;
+	private String m_addr;
+	private String m_detail_addr;
+	private int m_grade;
+	private int m_state;
+	private String m_reg_date;
+	private String m_mod_date;
+
+}
