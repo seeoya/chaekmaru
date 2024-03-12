6656fbfbe01bac00c29e0adfd0b784cea7392a56
seeoya <seeoya@naver.com>
2024-03-12 13:05:16 +0900

    코드 정리

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -4,35 +4,32 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     document.querySelectorAll("form.member").forEach((el) => {
-        el.addEventListener("submit", function (e) {
-        })
+        el.addEventListener("submit", function (e) {});
     });
 
     document.querySelectorAll("form.member input").forEach((el) => {
         el.addEventListener("keyup", function (e) {
             console.log(e);
             if (e.keyCode == 13) {
-
                 document.getElementById("submit_btn").click();
             }
         });
     });
 
-
-    document.querySelectorAll("button.submit").forEach(el => {
+    document.querySelectorAll("button.submit").forEach((el) => {
         el.addEventListener("click", function (e) {
             let form = e.target.closest("form");
 
             if (form.r_stars.value > 5 || form.r_stars.value < 1) {
-                alert("1~5 사이의 별점을 선택해 주세요.")
+                alert("1~5 사이의 별점을 선택해 주세요.");
                 form.r_stars.value = 1;
                 form.r_stars.focus();
-            } else if (form.r_text.value == '') {
-                alert("리뷰를 입력해 주세요.")
+            } else if (form.r_text.value == "") {
+                alert("리뷰를 입력해 주세요.");
                 form.r_text.focus();
             } else {
                 form.submit();
             }
-        })
-    })
+        });
+    });
 });
