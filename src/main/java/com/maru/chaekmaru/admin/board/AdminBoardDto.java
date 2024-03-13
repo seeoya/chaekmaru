99348b9fe1f10737de4f150d15a0c45fa5e2dd29
post50 <ppm4ppm4@gmail.com>
2024-03-13 11:57:15 +0900

    차트용 DTO

--- src/main/java/com/maru/chaekmaru/admin/board/AdminBoardDto.java
+++ src/main/java/com/maru/chaekmaru/admin/board/AdminBoardDto.java
@@ -5,10 +5,13 @@ import lombok.Data;
 @Data
 public class AdminBoardDto {
 	
-	private int ab_sales_weekly;
-	private String ab_sales_weekly_date;
-	private int ab_sales_cate;
-	private String ab_sales_cate_name;
+	private int sc_daily_sales;
+	private String sc_daily_sales_date;
+	private int sc_sales_cate;
+	private String sc_sales_cate_name;
+	private String sc_sales_cate_date;
+	private String sc_reg_date;
+	private String sc_mod_date;
 	
 	
 }
