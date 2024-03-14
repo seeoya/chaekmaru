2b23f0107e9742a36d423d9f01d12bd4c9114db1
post50 <ppm4ppm4@gmail.com>
2024-03-14 16:28:30 +0900

    차트

--- src/main/java/com/maru/chaekmaru/admin/AdminHomeController.java
+++ src/main/java/com/maru/chaekmaru/admin/AdminHomeController.java
@@ -1,10 +1,16 @@
 package com.maru.chaekmaru.admin;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.maru.chaekmaru.admin.board.AdminBoardDto;
+import com.maru.chaekmaru.admin.board.AdminBoardService;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.log4j.Log4j2;
@@ -18,9 +24,22 @@ public class AdminHomeController {
 	public String home() {
 		log.info("home()");
 		
-		String nextPage = "/admin/index";
+	/*	@Autowired
+		AdminBoardService adminBoardService;
 		
-		return nextPage;
+		@GetMapping({"/", ""})
+		public String adminBoardForm(Model model) {
+			log.info("adminBoardForm()");
+					
+			ArrayList<AdminBoardDto> salesdaily = adminBoardService.totalSalesDaily();
+			ArrayList<AdminBoardDto> salesCateDaily = adminBoardService.salesCateDaily();
+					
+			model.addAttribute("salesdaily", salesdaily);
+			model.addAttribute("salesCateDaily", salesCateDaily);*/
+			
+			String nextPage = "admin/index";
+			
+			return nextPage;		
 	}
 
 	@GetMapping("/result")
